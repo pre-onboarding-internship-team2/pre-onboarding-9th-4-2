7c1e2ce652a8facb155a13af5790ef48a791286e
ggsno <ogs0518@gmail.com>
2023-03-19 15:49:16 +0900

    feat: 주문목록 테이블 생성 #1

--- src/components/OrderTable.tsx
+++ src/components/OrderTable.tsx
@@ -5,6 +5,35 @@ export default function OrderTable() {
   const { data: orderList, isSuccess } = useQuery(["order"], fetchGetOrderList);
 
   return (
-    <>{isSuccess ? orderList.map((order) => order.customer_name) : "fail"}</>
+    <>
+      {isSuccess ? (
+        <table>
+          <thead>
+            <tr>
+              {Object.keys(orderList[0]).map((key) => (
+                <th key={key}>
+                  {key
+                    .replace("_", " ")
+                    .replace(/\b[a-z]/g, (c) => c.toUpperCase())}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {orderList.map((order) => (
+              <tr key={order.id}>
+                {Object.values(order).map((value) => (
+                  <td key={order.id.toString() + value.toString()}>
+                    {value.toString()}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        "fail"
+      )}
+    </>
   );
 }
