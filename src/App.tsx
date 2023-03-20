3785c9842cc29c59b04e99816d8704c6ad5d3ec3
iuesver <87600354+iuesver@users.noreply.github.com>
2023-03-20 23:05:37 +0900

    feat : 메인페이지 구현
    SWR과 Axios를 통한 데이터 처리 및 해당 데이터 렌더링 할 JSX 코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { createBrowserRouter, Outlet } from "react-router-dom";
 import "./App.css";
+import MainPage from "./pages/MainPage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+  },
+]);
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
