096b2bbd0527459fcbb5e41ec0b183c197ce17f8
onezeun <onezeun@gmail.com>
2023-03-21 03:41:41 +0900

    fix: order data 타입 수정

--- src/types/order.types.ts
+++ src/types/order.types.ts
@@ -1,7 +1,7 @@
 export interface OrderDataResponse {
   id: string;
-  transaction_time: number;
-  status: number;
+  transaction_time: string;
+  status: boolean;
   customer_id: number;
   customer_name: string;
   currency: string;
