72f74cfcb9a0d0c0d209f68e41ea263b6575c061
onezeun <onezeun@gmail.com>
2023-03-21 16:13:30 +0900

    refactor: 필터 컴포넌트 분리

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as S from '../styles/Main.styles';
+import Filter from 'components/Filter';
 import Table from 'components/Table';
 
 export default function Main() {
@@ -7,19 +8,7 @@ export default function Main() {
     <>
       <S.MainTitleContainer>
         <h2>오늘의 거래건 (2023-03-08)</h2>
-        <S.FilterWrap>
-          <span>주문처리상태</span>
-          <S.Select>
-            <option value="1">all</option>
-            <option value="1">true</option>
-            <option value="2">false</option>
-          </S.Select>
-          <S.SearchForm action="" method="get">
-            <label>고객명 검색</label>
-            <S.SearchInput type="text"></S.SearchInput>
-            <S.SearchButton type="submit">검색</S.SearchButton>
-          </S.SearchForm>
-        </S.FilterWrap>
+        <Filter />
       </S.MainTitleContainer>
       <Table />
     </>
