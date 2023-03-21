6b4fef34c310bdc01a19d0a23c8943c4a7f8740b
jiyeon <a1919361@gmail.com>
2023-03-22 01:49:37 +0900

    fix : formatDate 함수 수정
    - toISOString은 utc시간대로 표시된다. 잘못된 형태로 변환하고 있었다
    - 목록에 9일자 데이터가 있는 것을 확인하고 테스트코드 작성하여 수정함

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,4 +1,10 @@
 /** yyyy-mm-dd 형태로 반환 */
 export function formatDate(datestring: string): string {
-  return new Date(datestring).toISOString().slice(0, 10);
+  const dateObject = new Date(datestring);
+
+  const year = dateObject.getFullYear();
+  const month = dateObject.getMonth() + 1;
+  const day = dateObject.getDate();
+
+  return `${year}-${month < 10 && "0"}${month}-${day < 10 && "0"}${day}`;
 }
